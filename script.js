bf150b1eefad2a401c82826a18f82524e601c995
alejandrodavidvela <alex.d.vela@gmail.com>
2021-04-11 21:35:35 -0500

    updated for cp

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const search = document.getElementById('search');
 const submit = document.getElementById('submit');
 const random = document.getElementById('random');
-const chubbiesEl = document.getElementById('chubbies');
+const cryptopunksEl = document.getElementById('cryptopunks');
 const resultHeading = document.getElementById('result-heading');
-const single_chubbyEl = document.getElementById('single-chubby');
+const single_cryptopunkEl = document.getElementById('single-cryptopunk');
 const get_age = document.getElementById('get-age');
 
 
-function searchChubby(e){
+function searchCryptopunk(e){
     e.preventDefault();
 
-    // Clear Single Chubby
-    single_chubbyEl.innerHTML = '';
+    // Clear Single cryptopunk
+    single_cryptopunkEl.innerHTML = '';
 
     // Get Search Term
     const term = search.value;
@@ -49,27 +49,27 @@ function searchChubby(e){
                         
                         
 
-                        chubbiesEl.innerHTML = data.assets.map(asset => `
-                            <div class="chubby">
-                                <img src="${asset.image_url}" alt="${asset.name}" />
-                                <div class="chubby-info" data-chubbyID="${asset.id}">
+                        cryptopunksEl.innerHTML = data.assets.map(asset => `
+                            <div class="cryptopunk">
+                                <img class="cryptopunk-img" src="${asset.image_url}" alt="${asset.name}" />
+                                <div class="cryptopunk-info" data-cryptopunkID="${asset.id}">
                                     <h3>${asset.name}</h3>
                                 </div>
-                                <div class="chubby-birthday">
+                                <div class="cryptopunk-birthday">
                                     <h3>Age:</h3>
-                                    <h4 class="chubby-birthday-date">${diff} Yrs Old</h4>
+                                    <h4 class="cryptopunk-birthday-date">${diff} Yrs</h4>
                                 </div>
-                                <div class="chubby-owner">
+                                <div class="cryptopunk-owner">
                                     <h3>Owner:</h3>
-                                    <a class="chubby-address" href="https://etherscan.io/address/${asset.owner.address}">${asset.owner.address}</a>
+                                    <a class="cryptopunk-address" href="https://etherscan.io/address/${asset.owner.address}">${asset.owner.address}</a>
                                 </div>
-                                <div class="chubby-traits">
+                                <div class="cryptopunk-traits">
                                     <h3>Bid on NFT:</h3>
-                                    <a class="chubby-traits-link" href="https://opensea.io/assets/0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb/${term}">OpenSea</a>     
+                                    <a class="cryptopunk-traits-link" href="https://opensea.io/assets/0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb/${term}">OpenSea</a>     
                                 </div>
-                                <div class="chubby-namebase">
-                                    <h3>Bid on TLD:</h3>
-                                    <a class="chubby-namebase-link" href="https://www.namebase.io/domains/phb">Namebase</a>     
+                                <div class="cryptopunk-namebase">
+                                    <h3>Bid on .phb/</h3>
+                                    <a class="cryptopunk-namebase-link" href="https://www.namebase.io/domains/phb">Namebase</a>     
                                 </div>
                                 
     
@@ -89,4 +89,4 @@ function searchChubby(e){
 
 
 // Event Listener
-submit.addEventListener('submit', searchChubby);
+submit.addEventListener('submit', searchCryptopunk);
